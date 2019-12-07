9c5bc9a00358522384ffc8810fb5ae25b0faf7ee
terngkub <terngnattapol@gmail.com>
2019-12-07 12:05:03 +0100

    add boost test structure

---
+++ source/complex_test.cpp
@@ -0,0 +1,11 @@
+#include "complex.hpp"
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(complex_test)
+
+BOOST_AUTO_TEST_CASE(init)
+{
+    BOOST_TEST(true);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
